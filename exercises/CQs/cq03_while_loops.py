adffac5c93d988d76527220aadb59e79aa22ca43
jkaufunc <jkauf@ad.unc.edu>
2024-10-03 17:00:25 -0400

    ex03 cq03

--- exercises/CQs/cq03_while_loops.py
+++ exercises/CQs/cq03_while_loops.py
@@ -2,15 +2,20 @@ __author__ = "James Kaufman"
 
 
 def num_instances(phrase: str, search_char: str) -> int:
-    """This function counts the number of times a character occurs in a sequence of inputted strings."""
+    # This function counts the number of times
+    #   a character occurs in a sequence of inputted strings.
 
     count: int = 0
-    """tracks the number of characters in the inputted string: phrase."""
+    # tracks the number of characters in the inputted string: phrase.
 
     output: int = 0
-    """output is the integer that is updated as the function locates characters that match "search_char", and it will eventually be printed as the final return value."""
+    # output is the integer that is updated as the function locates characters that
+    # match "search_char", and it will eventually be printed as the final return value.
 
-    """the expression, "count < len(phrase)" is the boolean expression that will end the function. Once the count is equal to the len(phrase), we know all the characters in the phrase have been considered, and the while loop can be exitted."""
+    # the expression, "count < len(phrase)" is the boolean expression that will
+    #   end the function. Once the count is equal to the len(phrase), we know all
+    #       the characters in the phrase have been considered, and the while loop
+    #           can be exitted."""
     while count < len(phrase):
         if phrase[count] == search_char:
             output = output + 1
---
+++ exercises/ex03_wordle.py
@@ -0,0 +1,75 @@
+"""A recreation of the popular New York Times game, Wordle!"""
+
+__author__ = "730761420"
+
+
+def input_guess(guess_len: int) -> str:
+    (
+        """prompts the function caller for a word of the
+    inputted length"""
+    )
+
+    # at first, I kept trying to make the paramenter it's own local variable.
+    # Then I remembered I could assign a local variable to an input
+    # I didn't realize the length of the secret word was to be specified when calling
+    #   the input_guess function.
+
+    guess: str = input(f"Choose a {guess_len} letter word for your first guess: ")
+    while len(guess) != guess_len:
+        guess = input(
+            f"You must choose a {guess_len} letter word for your first guess: "
+        )
+
+    return guess
+
+
+def contains_char(secret_word: str, char_guess: str) -> bool:
+    """this function seraches a string for a specified character"""
+    assert len(char_guess) == 1
+
+    idx: int = 0
+
+    condition: bool = False
+
+    while idx < len(secret_word):
+        if secret_word[idx] == char_guess:
+            condition = True
+
+            idx += 1
+
+    return condition
+
+
+# for now, although I think my contains_char functoin should function, it isn't
+#   generating any output when I try to print a function call
+#   maybe that won't affect my later code and the final project will work?
+
+
+def emojified(guessed_word: str, secret_word: str) -> str:
+    (
+        """this function compares the guessed word to the
+    secret word which is the solution to the puzzle"""
+    )
+    assert len(guessed_word) == len(secret_word)
+
+    white_box: str = "\U00002B1C"
+    green_box: str = "\U0001F7E9"
+    yellow_box: str = "\U0001F7E8"
+
+    idx: int = 0
+
+    display: str = ""
+
+    while idx < len(secret_word):
+        if contains_char(secret_word, guessed_word[idx]):
+            display += yellow_box
+        elif contains_char(secret_word, guessed_word[idx]):
+            display += white_box
+        else:
+            display += green_box
+        idx += 1
+
+    return display
+
+
+print(emojified("hello", "melon"))
