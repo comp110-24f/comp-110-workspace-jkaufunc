8c7a450d9cbc9d7a21a9b222300192a6733cc0e8
jkaufunc <jkauf@ad.unc.edu>
2024-10-07 15:35:56 -0400

    for_in_loops

---
+++ .devcontainer/lessons/for_in_loops.py
@@ -0,0 +1,10 @@
+pets: list[str] = ["Louie", "Bo", "Bear"]
+
+# for pet_name in pets:
+# print(f"Good boy {pet_name}!")
+
+
+names: list[str] = ["Alyssa", "Janet", "Vrinda"]
+
+for name in range(0, len(names)):
+    print(f"{name}: {names[name]}")
