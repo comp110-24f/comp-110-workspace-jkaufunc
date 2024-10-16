9d0c2cef76850b8de6dffa6419b5e861767c9c10
jkaufunc <jkauf@ad.unc.edu>
2024-10-16 15:15:37 -0400

    cq07

--- CQs/cq07/max_test.py
+++ CQs/cq07/max_test.py
@@ -1,12 +1,16 @@
 from CQs.cq07.find_max import find_and_remove_max
 
 
-def test_find_and_remove_max() -> None:
+def test_find_and_remove_max_use_case_1() -> None:
     assert find_and_remove_max([1, 2, 3, 4, 5]) == 5
 
+
+def test_find_and_remove_max_use_case_2() -> None:
     test_list: list = [2, 4, 6, 8]
     find_and_remove_max(test_list)
 
     assert test_list == [2, 4, 6]
 
+
+def test_find_and_remove_max_edge_case() -> None:
     assert find_and_remove_max([]) == -1
