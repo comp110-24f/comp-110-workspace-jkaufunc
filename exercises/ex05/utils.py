5cfaea438e357674c365d0e16807249a2f5fe83f
jkaufunc <jkauf@ad.unc.edu>
2024-10-16 19:50:24 -0400

    ex05

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -32,3 +32,21 @@ def sub(list1: list[int], start_idx: int, end_index: int) -> list:
             new_list = new_list
 
     return new_list
+
+
+def add_at_index(list1: list[int], element: int, idx: int) -> None:
+    if idx < 0 or idx > len(list1):
+        raise IndexError("Index is out of bounds for the index list")
+    else:
+        temp_list: list = []
+
+        for nums in range(idx, len(list1)):
+            temp_list.append(nums)
+
+        for nums in range(idx, len(list1)):
+            list1.pop(nums)
+
+        list1.append(element)
+
+        for nums in range(0, len(temp_list)):
+            list1.append(temp_list[nums])
