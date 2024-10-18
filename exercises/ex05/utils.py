e6897e29434d88b3915f8bff6c9e2d84402fdbc8
jkaufunc <jkauf@ad.unc.edu>
2024-10-18 00:08:28 -0400

    ex05

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -18,7 +18,8 @@ def only_evens(list1: list[int]) -> list:
 def sub(list1: list[int], start_idx: int, end_idx: int) -> list:
     (
         """this function creates a shortened list from the inputted list bounded by
-        a given start and end index"""
+        a given start and end index. The start index is included, while the end index
+        is excluded"""
     )
 
     new_list: list[int] = []
@@ -35,7 +36,7 @@ def sub(list1: list[int], start_idx: int, end_idx: int) -> list:
                     new_list = new_list
         elif end_idx > len(list1):
             for num in range(0, len(list1)):
-                if num >= start_idx and num < end_idx:
+                if num >= start_idx and num < len(list1):
                     new_list.append(list1[num])
                 else:
                     new_list = new_list
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,32 +1,61 @@
 from exercises.ex05.utils import only_evens, sub, add_at_index
 import pytest
 
+__author__ = "730761420"
+
 
 def test_only_evens_usecase_1() -> None:
+    (
+        """basic test to see if the function only_evens
+    removes all the odd numbers in the list"""
+    )
     assert only_evens([1, 2, 3, 4, 5, 6]) == [2, 4, 6]
 
 
 def test_only_evens_edgecase() -> None:
+    (
+        """test to confirm that an empty list inputted to
+    only_evens will return and empty list"""
+    )
     assert only_evens([]) == []
 
 
 def test_only_evens_usecase_2() -> None:
+    (
+        """test to confirm that only_evens will return an empty
+     list when given a list of only odd numbers"""
+    )
     assert only_evens([1, 3, 5]) == []
 
 
 def test_sub_caseuse_1() -> None:
+    """test of the basic purpose and use of the function"""
     assert sub([1, 2, 3, 4, 5], 1, 4) == [2, 3, 4]
 
 
 def test_sub_caseuse_2() -> None:
+    (
+        """test to show that inputting a start and end index out of the bounds of the
+    indices of the list will return the whole inputted list"""
+    )
     assert sub([1, 2, 3, 4, 5], -1, 5) == [1, 2, 3, 4, 5]
 
 
+def test_sub_caseuse_3() -> None:
+    assert sub([1, 2, 3, 4, 5], -1, 4) == [1, 2, 3, 4]
+
+
+def test_sub_caseuse_4() -> None:
+    assert sub([1, 2, 3, 4, 5], 2, 6) == [3, 4, 5]
+
+
 def test_sub_edgecase() -> None:
+    """empty list will return an empty list"""
     assert sub([], 1, 4) == []
 
 
 def test_add_at_index_caseuse_1() -> None:
+    """testing the insertion of a number into the middle of the list"""
     test_list: list = [1, 2, 4]
     add_at_index(test_list, 3, 2)
     assert test_list == [1, 2, 3, 4]
@@ -43,6 +72,7 @@ def test_add_at_index_raises_indexerror():
 
 
 def test_add_at_index_caseuse_2() -> None:
+    """testing the insertion of a number to the end of the list"""
     test_list: list = [2, 4, 6]
     add_at_index(test_list, 8, 3)
     assert test_list == [2, 4, 6, 8]
